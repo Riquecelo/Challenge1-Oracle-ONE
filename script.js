9a04885a68dc5ead09a0d378afb110a7787a4778
Marcelo Santos <riquecelox462m@gmail.com>
2022-01-22 10:46:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-/*A letra "e" é convertida para "enter"
-A letra "i" é convertida para "imes"
-A letra "a" é convertida para "ai"
-A letra "o" é convertida para "ober"
-A letra "u" é convertida para "ufat"*/
 
+/*********  tratamento de Criptografia **********/
+function criptografaTexto() {
 
-function pegaTexto() {
-
-    var textoCapturado = document.querySelector('input').value;
+    let textoCapturado = document.querySelector('input').value;
     //console.log(textoCapturado);
     //console.log(textoCapturado.split(''));
-    var arrayTexto = textoCapturado.split('');
-    var textoCriptografado;
-
+    let arrayTexto = textoCapturado.split('');
+    let textoCriptografado;
 
     for (var i = 0; i < arrayTexto.length; i++) {
 
@@ -34,19 +28,49 @@ function pegaTexto() {
     }
 
     textoCriptografado = arrayTexto.join('');
-    console.log(textoCriptografado);
+    //console.log(textoCriptografado);
 
     let exibeCripto = document.getElementById('msg');
-    console.log(exibeCripto)
+    //console.log(exibeCripto)
     exibeCripto.value = textoCriptografado;
-
 }
 
+/* evento de click do botão criptografar */
+var btnCripto = document.querySelector('button');
 
-var btn = document.querySelector('button');
-
-btn.addEventListener('click', function (e) {
+btnCripto.addEventListener('click', function (e) {
     e.preventDefault();
 
-    pegaTexto();
+    criptografaTexto();
+})
+
+/******** Tratamendo de Descriptografia *********/
+function converteTexto(textoCapturado){
+    
+    return textoCapturado.replaceAll('ai','a').replaceAll('enter','e').replaceAll('ims','i').replaceAll('ober','o').replaceAll('ufat','u');
+/*
+   return textoCapturado.replace(/ai/g,'a').replace(/enter/g,'e').replace(/ims/g,'i').replace(/ober/g,'o').replace(/ufat/g,'u');*/  
+    
+}
+
+/* evento de click do botão descriptografar */
+var btnDescripografar = document.getElementById('btn-descripto');
+
+btnDescripografar.addEventListener('click', function(e){
+    e.preventDefault();
+
+    let textoCapturado = document.getElementById('input-texto').value;
+    
+    let exibeDesCripto = document.getElementById('msg');
+    //console.log(exibeDesCripto)
+    exibeDesCripto.value = converteTexto(textoCapturado);
+})
+
+/*****  função copiar ******/
+var btnCopy = document.getElementById('copiar');
+
+btnCopy.addEventListener('click', function(e){
+    e.preventDefault();
+    document.getElementById('msg').select();
+    document.execCommand('copy');
 })
